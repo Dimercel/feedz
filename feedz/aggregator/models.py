f5ae9259c641663cc0fb12b091c81b3ca5a73b65
Dimercel <xolcman@gmail.com>
2020-10-07 16:44:48 +0300

    Create relations between basic models

--- feedz/aggregator/models.py
+++ feedz/aggregator/models.py
@@ -5,14 +5,6 @@ class Category(models.Model):
     name = models.CharField(max_length=50, unique=True, null=False)
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=250, null=False)
-    url = models.URLField(max_length=500, null=False)
-    favorite = models.BooleanField(default=False)
-    published = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
 class Channel(models.Model):
     url = models.URLField(max_length=500, unique=True, null=False)
     name = models.CharField(max_length=50, null=False)
@@ -20,3 +12,15 @@ class Channel(models.Model):
     last_seen = models.DateTimeField()
     last_sync = models.DateTimeField()
     post_limit = models.PositiveSmallIntegerField(default=100)
+
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, null=False)
+    url = models.URLField(max_length=500, null=False)
+    favorite = models.BooleanField(default=False)
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    channel = models.ForeignKey(Channel, null=True, on_delete=models.CASCADE)
