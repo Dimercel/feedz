d172c7336b72ea68ca2a65735c43e87eab64de42
Dimercel <xolcman@gmail.com>
2020-11-07 21:10:13 +0300

    Add login_required decorator

--- feedz/aggregator/urls.py
+++ feedz/aggregator/urls.py
@@ -1,11 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('category/', views.CategoryListView.as_view(), name='category-list'),
-    path('category/new/', views.CategoryCreate.as_view(), name='category-new'),
-    path('category/<pk>/update/', views.CategoryUpdate.as_view(), name='category-update'),
+    path('category/',
+         login_required(views.CategoryListView.as_view()),
+         name='category-list'),
 
-    path('channel/new/', views.ChannelCreate.as_view(), name='channel-new'),
+    path('category/new/',
+         login_required(views.CategoryCreate.as_view()),
+         name='category-new'),
+
+    path('category/<pk>/update/',
+         login_required(views.CategoryUpdate.as_view()),
+         name='category-update'),
+
+    path('channel/new/',
+         login_required(views.ChannelCreate.as_view()),
+         name='channel-new'),
 ]
