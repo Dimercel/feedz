e48f73eaed383d128366e22b71ad390747e75fad
Dimercel <xolcman@gmail.com>
2020-11-13 19:18:41 +0300

    feed sync action

---
+++ feedz/aggregator/feed.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+import feedparser
+
+from .models import Post
+
+
+def sync_feed(channel):
+    d = feedparser.parse(channel.url)
+    for item in d.entries:
+        if Post.objects.filter(title=item.title, channel=channel).exists():
+            break
+
+        Post.objects.create(
+            title=item.title,
+            url=item.link,
+            favorite=False,
+            published=True,  # FIXME
+            channel=channel,
+        )
+
+    channel.last_sync = datetime.utcnow()
+    channel.save()
+
+    post_count = Post.objects.filter(channel=channel).count()
+    if post_count > channel.post_limit:
+        to_delete = (Post.objects.all().order_by('created_at')
+                     [:post_count - channel.post_limit])
+        to_delete.delete()
