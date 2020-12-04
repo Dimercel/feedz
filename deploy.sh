36674d59bb0dbdb8003b8715d469c6997efb744f
Dimercel <xolcman@gmail.com>
2020-12-04 19:20:58 +0300

    Deploy script corrections

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ git pull
 pip3 install -r requirements.txt
 
 # Update database, static files, locales
-manage.py syncdb  --noinput
-manage.py migrate
-manage.py collectstatic --noinput
-manage.py makemessages -a
-manage.py compilemessages
+python3 manage.py syncdb  --noinput
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+python3 manage.py makemessages -a
+python3 manage.py compilemessages
 
 # restart wsgi
-touch project_name/wsgi.py
+# touch project_name/wsgi.py
